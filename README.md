c412efc7dbfb585b693236d15f25d0aaeee7297c
Overcooked Panda <overcookedpanda@gmail.com>
2020-05-08 15:22:09 -0500

    initial commit for veriblock-pop-cpp vcpkg module

---
+++ README.md
@@ -0,0 +1,17 @@
+# alt-integration-cpp-vcpkg
+
+How to use:
+
+1. Install vcpkg first.
+2. Add repository as submodule with the name 'veriblock-pop-cpp'
+```
+$ cd vcpkg/ports
+# cloned directory name should be 'veriblock-pop-cpp'
+$ git submodule add --name veriblock-pop-cpp https://github.com/VeriBlock/alt-integration-cpp-vcpkg veriblock-pop-cpp
+```
+
+2. Use vcpkg:
+```
+$ vcpkg install veriblock-pop-cpp
+```
+ 
---
+++ portfile.cmake
@@ -0,0 +1,35 @@
+include(vcpkg_common_functions)
+
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO VeriBlock/alt-integration-cpp
+        REF d8599ec678766d6f44883bde26ecbec3f011a779
+        HEAD_REF master
+        SHA512 f16e75b33a477e46d5946a990f3d4dc9bfb414bf0900aa3debc51944894892f560706bae989566a3e832d8880435e6467ca3f4221e988a6f63ca61bc418c212e
+        HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+        SOURCE_PATH ${SOURCE_PATH}
+        PREFER_NINJA
+        OPTIONS
+        -DUBSAN:BOOL=OFF
+        -DTSAN:BOOL=OFF
+        -DASAN:BOOL=OFF
+        -DSHARED:BOOL=OFF
+        -DTESTING:BOOL=OFF
+        -DWITH_SECP256K1:BOOL=OFF
+        -DCMAKE_DEBUG_POSTFIX=d
+)
+
+vcpkg_install_cmake()
+
+file(COPY ${CURRENT_PACKAGES_DIR}/debug/lib/cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/)
+file(COPY ${CURRENT_PACKAGES_DIR}/lib/cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/cmake")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
