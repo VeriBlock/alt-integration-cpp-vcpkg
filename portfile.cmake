0044d612798ef30f0276e92145216e73aa28cef3
GitHub Action <action@github.com>
2020-05-22 11:35:44 +0000

    Update veriblock-pop-cpp version

--- portfile.cmake
+++ portfile.cmake
@@ -5,8 +5,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO VeriBlock/alt-integration-cpp
-        REF d8964ec757a40a2c8b49658d59aa88f00303e7ca
-        SHA512 5b3cdaca5e913e038614cc032788def1fd17537c01c7fe9c083461967554a7ce589f735843e26c4cc651eb48e084f60183503789af3fdeeab6a5749f8fb3b3c1
+        REF daef0718af65c0eda54aea6dbeaa2d11f64b471a
+        SHA512 bb3761d2d2038a3e41f3aadb11fecf9b002d864523b866e415d14c9239c7309b7afe2aa42b1f267b354791959bda3d550f89143199d8aacd229afec1f0b3dc69
         HEAD_REF master
 )
 
