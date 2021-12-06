65b6888bc0b9f7ced95f5d3f8767a1add0953ee4
vbk-deploy <vbk-deploy@users.noreply.github.com>
2021-12-06 18:05:41 +0000

    Update veriblock-pop-cpp version

--- portfile.cmake
+++ portfile.cmake
@@ -5,8 +5,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO VeriBlock/alt-integration-cpp
-        REF 3eaaf0706e51c128c3b9b275fcf16a25e765f828
-        SHA512 0f81b831d7364c8d20c51e06de96de0a77afdd1003a2a93046bcd405bf0eeb698dfc11e999e4ddcbf05b3afe9816dbb1a0efa01d9a6d91d36ab84ab0ece4f94d
+        REF 7b0b1ee1785c89c3813cb5bd9118a115422954c6
+        SHA512 df13547dbeeffb12ae2ef5b4120d9b98c097d25e6cb11476ea892b007bd37f3ce6e62e877bee6f1d1ab37ab48b5bfa023c3aa0cd6d6f0586ac2881aa6dff077e
         HEAD_REF master
 )
 
